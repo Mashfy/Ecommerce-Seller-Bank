7d695b2ac6302ce9a001b9ae1db37a9689c89d35
Mashfy <badrulalamsiddique@gmail.com>
2022-06-02 02:36:04 +0600

    temporary seeder to work with data

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,11 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
       useUnifiedTopology: true,
+      useNewUrlParser: true,
     });
-    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
