e57a292e86956ca4f44937dc18c30d69189eaf56
Mashfy <badrulalamsiddique@gmail.com>
2022-06-04 21:49:04 +0600

    cartscreen added

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -4,6 +4,7 @@ import {
   useParams,
   useLocation,
   useSearchParams,
+  useNavigate,
 } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -14,6 +15,7 @@ import {
   Form,
   Button,
   Card,
+  ListGroupItem,
 } from 'react-bootstrap';
 import Message from '../components/Message';
 import { addToCart } from '../actions/cartActions';
@@ -22,6 +24,7 @@ const CartScreen = () => {
   const { id } = useParams();
   const { search } = useLocation();
   const [searchParms] = useSearchParams();
+  const navigate = useNavigate();
 
   const productID = id;
   const qty = search ? Number(search.split('=')[1]) : 1;
@@ -37,6 +40,14 @@ const CartScreen = () => {
     }
   }, [dispatch, productID, qty]);
 
+  const removeFromCartHandler = (id) => {
+    console.log('remove');
+  };
+
+  const checkoutHandler = () => {
+    navigate('/login?redirect=shipping');
+  };
+
   return (
     <Row>
       <Col md={8}>
@@ -50,11 +61,86 @@ const CartScreen = () => {
             </Link>
           </Message>
         ) : (
-          <ListGroup variant='flush'></ListGroup>
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Link to={`/product/${item.product}`}>
+                      <Image src={item.image} alt={item.name} fluid rounded />
+                    </Link>
+                  </Col>
+                  <Col md={3}>
+                    <Link
+                      to={`/product/${item.product}`}
+                      style={{
+                        textDecoration: 'none',
+                        fontWeight: 'bold',
+                        color: 'black',
+                      }}
+                    >
+                      {item.name}
+                    </Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as='select'
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
         )}
       </Col>
-      <Col md={2}></Col>
-      <Col md={2}></Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type='button'
+                classname='btn-block'
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Go to Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
     </Row>
   );
 };
