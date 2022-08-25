c5844600b7b41ba09c355480227395ac9a09c14c
Mashfy <badrulalamsiddique@gmail.com>
2022-08-25 22:06:38 +0600

    file upload bug fixed

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,8 @@ app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+// const __dirname = path.resolve();
+// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
