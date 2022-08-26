d1b6e0730f29f0e3b0c9967288271b512ba9748c
Mashfy <badrulalamsiddique@gmail.com>
2022-08-26 22:54:25 +0600

    multiple bug fixed

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,25 +1,25 @@
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
 
 const users = [
-    {
-      name: 'Admin',
-      email: 'admin@gmail.com',
-      account_number: '111423192491172311192292132513',
-      password: bcrypt.hashSync('password', 10),
-      isAdmin: true,
-    },
-    {
-      name: 'Junaed',
-      email: 'junaed@gmail.com',
-      account_number: '20312411151491172311192292132513',
-      password: bcrypt.hashSync('password', 10),
-    },
-    {
-      name: 'Omar',
-      email: 'Omar@gmail.com',
-      account_number: '2523112891172311192292132513',
-      password: bcrypt.hashSync('password', 10),
-    },
-  ]
-  
-  export default users
+  {
+    name: 'Admin',
+    email: 'admin@gmail.com',
+    account_number: '111423192491172311192292132513',
+    password: bcrypt.hashSync('password', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Alam',
+    email: 'alam@gmail.com',
+    account_number: '1122112391172311192292132523',
+    password: bcrypt.hashSync('password', 10),
+  },
+  {
+    name: 'Badrul',
+    email: 'badrul@gmail.com',
+    account_number: '12111428312291172311192292132523',
+    password: bcrypt.hashSync('password', 10),
+  },
+];
+
+export default users;
