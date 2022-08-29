19e8f22e783b81f974ea796835f4280fc26977a9
Mashfy <badrulalamsiddique@gmail.com>
2022-08-29 20:36:20 +0600

    supplier mail changed

--- backend_seller/data/users.js
+++ backend_seller/data/users.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 const users = [
   {
     name: 'Supplier',
-    email: 'seller@gmail.com',
+    email: 'supplier@gmail.com',
     account_number: '29152222152891172311192292132523',
     password: bcrypt.hashSync('123456', 10),
     isSeller: true,
