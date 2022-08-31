1d2373181f81ae052c572a7f3ebf9d7833c47e92
Mashfy <badrulalamsiddique@gmail.com>
2022-08-31 22:22:44 +0600

    screenshot updated

--- frontend_ecom/src/screens/CartScreen.js
+++ frontend_ecom/src/screens/CartScreen.js
@@ -59,7 +59,7 @@ const CartScreen = () => {
                   <Col md={3}>
                     <Link to={`/product/${item.product}`}>{item.name}</Link>
                   </Col>
-                  <Col md={2}>${item.price * 1.1}</Col>
+                  <Col md={2}>${(item.price * 1.1).toFixed(3)}</Col>
                   <Col md={2}>
                     <Form.Control
                       as='select'
