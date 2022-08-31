bb03d6f8679550da1bb3e1182b3528585e1087ff
Mashfy <badrulalamsiddique@gmail.com>
2022-08-31 23:11:30 +0600

    minor changes

--- backend_ecom/data/users.js
+++ backend_ecom/data/users.js
@@ -8,25 +8,25 @@ const users = [
     password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
-  {
-    name: 'John',
-    email: 'john@gmail.com',
-    account_number: '2025182491172311192292132523',
-    password: bcrypt.hashSync('123456', 10),
-  },
-  {
-    name: 'Jack',
-    email: 'jack@gmail.com',
-    account_number: '2011132191172311192292132523',
-    password: bcrypt.hashSync('123456', 10),
-  },
-  {
-    name: 'Supplier',
-    email: 'supplier@gmail.com',
-    account_number: '2011132191172311192292132523',
-    password: bcrypt.hashSync('123456', 10),
-    isAdmin: true,
-  },
+  // {
+  //   name: 'John',
+  //   email: 'john@gmail.com',
+  //   account_number: '2025182491172311192292132523',
+  //   password: bcrypt.hashSync('123456', 10),
+  // },
+  // {
+  //   name: 'Jack',
+  //   email: 'jack@gmail.com',
+  //   account_number: '2011132191172311192292132523',
+  //   password: bcrypt.hashSync('123456', 10),
+  // },
+  // {
+  //   name: 'Supplier',
+  //   email: 'supplier@gmail.com',
+  //   account_number: '2011132191172311192292132523',
+  //   password: bcrypt.hashSync('123456', 10),
+  //   isAdmin: true,
+  // },
 ];
 
 export default users;
