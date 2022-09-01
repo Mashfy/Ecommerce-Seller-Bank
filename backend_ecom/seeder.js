11c8b0eb0390edeec4ff1cc7168efc273226d250
Mashfy <badrulalamsiddique@gmail.com>
2022-09-01 16:30:50 +0600

    seeder updated

--- backend_ecom/seeder.js
+++ backend_ecom/seeder.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import users from './data/users.js';
 import products from './data/products.js';
 import User from './models/userModel.js';
-// import Product from './models/productModel.js';
+import Product from './models/productModel.js';
 import Order from './models/orderModel.js';
 import connectDB from './config/db.js';
 
@@ -14,7 +14,7 @@ await connectDB();
 const importData = async () => {
   try {
     await Order.deleteMany();
-    // await Product.deleteMany();
+    await Product.deleteMany();
     await User.deleteMany();
 
     const createdUsers = await User.insertMany(users);
