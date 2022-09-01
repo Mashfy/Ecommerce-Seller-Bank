97b7083bdaf26afcc5fd3dd3dca2fa92c998cd06
Mashfy <badrulalamsiddique@gmail.com>
2022-09-01 14:06:08 +0600

    minor changes

--- backend_ecom/data/users.js
+++ backend_ecom/data/users.js
@@ -4,7 +4,7 @@ const users = [
   {
     name: 'Admin',
     email: 'admin@gmail.com',
-    account_number: '2011132191172311192292132523',
+    account_number: '111423192491172311192292132523',
     password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
